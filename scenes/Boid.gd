b9b045530e935b74fb921e6c21132612ea2e98e4
Jason Chin <jason.rob.chin@gmail.com>
2021-09-26 01:25:21 -0400

    working without alignmnet

--- scenes/Boid.gd
+++ scenes/Boid.gd
@@ -2,42 +2,85 @@ extends Position2D
 
 class_name Boid
 
-export var cohesion_strength := 1.0
+export var cohesion_strength := 0.01
 export var separation_strength := 1.0
-export var alignment_strength := 1.0
+export var separation_distance := 100
+export var alignment_strength := 0.1
 export var vision_distance := 200
+export var max_speed := 10
 
+var draw := false
 var velocity: Vector2
+var vel_s := Vector2.ZERO
+var vel_c := Vector2.ZERO
+var vel_a := Vector2.ZERO
+var vel_b := Vector2.ZERO
+
+var sb_pos = []
 
 
-func move(delta: float, boids: Array):
-	velocity = velocity + cohesion(boids)
+func _draw():
+	if draw:
+		for pos in sb_pos:
+			draw_line(Vector2.ZERO, to_local(pos), Color.green, 1, true)
+
+		draw_line(Vector2.ZERO, vel_s, Color.purple, 1, true)
+		draw_line(Vector2.ZERO, vel_c, Color.orange, 1, true)
+		draw_line(Vector2.ZERO, vel_a, Color.white, 1, true)
+		draw_line(Vector2.ZERO, vel_b, Color.black, 1, true)
+
+
+func _process(_delta):
+	update()
+
+
+func move(delta: float, boids: Array, limits: Dictionary):
+	var _influence = influence(boids, limits)
+	velocity = lerp(velocity, velocity + _influence * delta, 0.5)
+	if velocity.length() > max_speed:
+		velocity = velocity.normalized() * max_speed
+	$Sprite.rotation = velocity.angle() + PI / 2
 	position = position + velocity * delta
 
 
-func cohesion(boids: Array) -> Vector2:
+func influence(boids: Array, limits: Dictionary) -> Vector2:
 	var center_of_mass := Vector2.ZERO
+	var avg_velocity := Vector2.ZERO
+	vel_s = Vector2.ZERO
+	sb_pos = []
+
 	for boid in boids:
 		if boid == self:
 			continue
+
 		center_of_mass += boid.position
-	center_of_mass /= boids.size()
+		avg_velocity += boid.velocity
 
-	var vel_c := center_of_mass - position
-	return vel_c * cohesion_strength
+		var vec_to = boid.position - position
+		if vec_to.length() < separation_distance:
+			sb_pos.append(boid.position)
+			vel_s -= vec_to * separation_distance / vec_to.length()
 
+	var n_boids := boids.size() - 1
+	center_of_mass /= n_boids
+	avg_velocity /= n_boids
 
-func separation(boids: Array) -> Vector2:
-	return Vector2.ZERO
+	vel_c = center_of_mass - position
+	vel_c *= cohesion_strength
+	vel_a = avg_velocity - velocity
+	vel_a *= alignment_strength
+	vel_s *= separation_strength
 
+	vel_b = Vector2.ZERO
+	var b_str = 1000
+	if position.x < limits["x_min"]:
+		vel_b += Vector2.RIGHT * b_str
+	elif position.x > limits["x_max"]:
+		vel_b += Vector2.LEFT * b_str
 
-func alignment(boids: Array) -> Vector2:
-	return Vector2.ZERO
+	if position.y < limits["y_min"]:
+		vel_b += Vector2.DOWN * b_str
+	elif position.y > limits["y_max"]:
+		vel_b += Vector2.UP * b_str
 
-
-func update_velocity(boids: Array) -> Vector2:
-	return Vector2.ZERO
-
-
-func update_rotation(boids: Array) -> Vector2:
-	return Vector2.ZERO
+	return vel_c + vel_a + vel_s + vel_b
--- scenes/BoidGroup.gd
+++ scenes/BoidGroup.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export var initial_boids: int = 20
+export var draw_debug: bool = false
 
 var rng := RandomNumberGenerator.new()
 var boid_scene = preload("res://scenes/Boid.tscn")
@@ -21,19 +22,21 @@ func _ready():
 
 func _process(delta: float):
 	for boid in boids:
-		boid.move(delta, boids)
+		boid.move(delta, boids, limits)
 
 
 func spawn_random_boids(n: int, x_min: int, x_max: int, y_min: int, y_max: int):
 	for _i in range(n):
-		var boid := spawn_boid(rng.randi_range(x_min, x_max), rng.randi_range(y_min, y_max))
+		var boid := spawn_boid(
+			rng.randi_range(x_min, x_max), rng.randi_range(y_min, y_max), draw_debug
+		)
 		boids.append(boid)
 
 
-func spawn_boid(x: int, y: int) -> Boid:
+func spawn_boid(x: int, y: int, draw: bool = false) -> Boid:
 	var boid_instance := boid_scene.instance() as Boid
+	boid_instance.draw = draw
 	boid_instance.global_position = Vector2(x, y)
-	boid_instance.rotate(rng.randf_range(0, 2 * PI))
-	boid_instance.velocity = Vector2.UP.rotated(boid_instance.rotation) * 10
+	boid_instance.velocity = Vector2.UP.rotated(rng.randf_range(0, 2 * PI)) * 1000
 	add_child(boid_instance)
 	return boid_instance
