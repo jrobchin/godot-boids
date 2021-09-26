fb467480124bf134d8e21c1394ed629733c9b2b5
Jason Chin <jason.rob.chin@gmail.com>
2021-09-25 23:15:49 -0400

    random boids moving forward

--- scenes/Boid.gd
+++ scenes/Boid.gd
@@ -1,3 +1,30 @@
 extends Position2D
 
 class_name Boid
+
+var velocity: Vector2
+
+
+func move(delta: float, boids: Array):
+	# velocity = update_velocity(boids) * delta
+	position = position + velocity * delta
+
+
+func cohesion(boids: Array) -> Vector2:
+	return Vector2.ZERO
+
+
+func separation(boids: Array) -> Vector2:
+	return Vector2.ZERO
+
+
+func alignment(boids: Array) -> Vector2:
+	return Vector2.ZERO
+
+
+func update_velocity(boids: Array) -> Vector2:
+	return Vector2.ZERO
+
+
+func update_rotation(boids: Array) -> Vector2:
+	return Vector2.ZERO
--- scenes/BoidGroup.gd
+++ scenes/BoidGroup.gd
@@ -1,19 +1,40 @@
 extends Node2D
 
+export var initial_boids: int = 20
+
 var rng := RandomNumberGenerator.new()
 var boid_scene = preload("res://scenes/Boid.tscn")
 
-# Called when the node enters the scene tree for the first time.
+var boids = []
+
+onready var viewport_size := get_viewport_rect().size
+onready var limits := {"x_min": 0, "x_max": viewport_size.x, "y_min": 0, "y_max": viewport_size.y}
+
+
 func _ready():
 	rng.randomize()
 
+	spawn_random_boids(
+		initial_boids, limits["x_min"], limits["x_max"], limits["y_min"], limits["y_max"]
+	)
 
-func _input(event):
-	if event.is_action_pressed("debug_1"):
-		spawn_boid(rng.randi_range(0, 1024), rng.randi_range(0, 600))
+
+func _process(delta: float):
+	for boid in boids:
+		boid.move(delta, boids)
 
 
-func spawn_boid(x: int, y: int):
+func spawn_random_boids(n: int, x_min: int, x_max: int, y_min: int, y_max: int):
+	for _i in range(n):
+		var boid := spawn_boid(rng.randi_range(x_min, x_max), rng.randi_range(y_min, y_max))
+		boids.append(boid)
+
+
+func spawn_boid(x: int, y: int) -> Boid:
 	var boid_instance := boid_scene.instance() as Boid
 	boid_instance.global_position = Vector2(x, y)
+	boid_instance.rotate(rng.randf_range(0, 2 * PI))
+	boid_instance.velocity = Vector2.UP.rotated(boid_instance.rotation) * 10
+
 	add_child(boid_instance)
+	return boid_instance
