03e0272269fb160b1a6c9cacd37c55641026de5d
Jason Chin <jason.rob.chin@gmail.com>
2021-09-27 01:13:03 -0400

    area boids

--- scenes/Boid.gd
+++ scenes/Boid.gd
@@ -11,46 +11,56 @@ export var border_strength := 350
 export var impulse := 0.5
 export var max_speed := 10
 
-var draw := false
-var velocity: Vector2
-var vel_s := Vector2.ZERO
-var vel_c := Vector2.ZERO
-var vel_a := Vector2.ZERO
-var vel_b := Vector2.ZERO
+var velocity := Vector2.ZERO
+var limits: Dictionary
 
-# func _draw():
-# 	if draw:
-# 		for pos in sb_pos:
-# 			draw_line(Vector2.ZERO, to_local(pos), Color.green, 1, true)
-
-# 		draw_line(Vector2.ZERO, vel_s, Color.purple, 1, true)
-# 		draw_line(Vector2.ZERO, vel_c, Color.orange, 1, true)
-# 		draw_line(Vector2.ZERO, vel_a, Color.white, 1, true)
-# 		draw_line(Vector2.ZERO, vel_b, Color.black, 1, true)
+var _positions := []
+var _neighbours := []
+var _vel_c := Vector2.ZERO
+var _vel_s := Vector2.ZERO
+var _vel_a := Vector2.ZERO
+var _vel_b := Vector2.ZERO
 
 
-func _process(_delta):
+func _draw():
+	draw_line(Vector2.ZERO, _vel_c, Color.red)
+	draw_line(Vector2.ZERO, _vel_s, Color.green)
+	draw_line(Vector2.ZERO, _vel_a, Color.blue)
+	draw_line(Vector2.ZERO, _vel_b, Color.yellow)
+	for i in range(_positions.size() - 1):
+		draw_line(to_local(_positions[i]), to_local(_positions[i + 1]), Color.cyan, 1, true)
+
+
+func _process(delta):
+	move(delta)
+	_positions.append(position)
+	if _positions.size() > 250:
+		_positions.pop_front()
 	update()
 
 
-func move(delta: float, boids: Array, limits: Dictionary):
-	var _influence = influence(boids, limits)
+func move(delta: float):
+	var _influence = influence()
 	velocity = lerp(velocity, velocity + _influence * delta, impulse)
 	if velocity.length() > max_speed:
 		velocity = velocity.normalized() * max_speed
+
 	$Sprite.rotation = velocity.angle() + PI / 2
+
 	position = position + velocity * delta
 
 
-func influence(boids: Array, limits: Dictionary) -> Vector2:
+func influence() -> Vector2:
+	_vel_c = Vector2.ZERO
+	_vel_s = Vector2.ZERO
+	_vel_a = Vector2.ZERO
+	_vel_b = Vector2.ZERO
+
 	var center_of_mass := Vector2.ZERO
 	var avg_velocity := Vector2.ZERO
-	vel_s = Vector2.ZERO
-	vel_a = Vector2.ZERO
-
 	var n_boids_align := 0
 
-	for boid in boids:
+	for boid in _neighbours:
 		if boid == self:
 			continue
 
@@ -63,30 +73,42 @@ func influence(boids: Array, limits: Dictionary) -> Vector2:
 			avg_velocity += boid.velocity
 
 		if vec_to.length() < separation_distance:
-			vel_s -= vec_to * separation_distance / vec_to.length()
+			_vel_s -= vec_to * separation_distance / vec_to.length()
 
-	var n_boids := boids.size() - 1
-	center_of_mass /= n_boids
-
-	vel_c = center_of_mass - position
-	vel_c *= cohesion_strength
+	if _neighbours.size() > 1:
+		center_of_mass /= _neighbours.size() - 1
+		_vel_c = center_of_mass - position
 
 	if n_boids_align > 0:
 		avg_velocity /= n_boids_align
-		vel_a = avg_velocity - velocity
+		_vel_a = avg_velocity - velocity
 
-	vel_a *= alignment_strength
-	vel_s *= separation_strength
+	_vel_c *= cohesion_strength
+	_vel_a *= alignment_strength
+	_vel_s *= separation_strength
 
-	vel_b = Vector2.ZERO
-	if position.x < limits["x_min"]:
-		vel_b += Vector2.RIGHT * border_strength
-	elif position.x > limits["x_max"]:
-		vel_b += Vector2.LEFT * border_strength
+	_vel_b = Vector2.ZERO
+	if position.x < limits.get("x_min", -INF):
+		_vel_b += Vector2.RIGHT * border_strength
+	elif position.x > limits.get("x_max", INF):
+		_vel_b += Vector2.LEFT * border_strength
 
-	if position.y < limits["y_min"]:
-		vel_b += Vector2.DOWN * border_strength
-	elif position.y > limits["y_max"]:
-		vel_b += Vector2.UP * border_strength
+	if position.y < limits.get("y_min", -INF):
+		_vel_b += Vector2.DOWN * border_strength
+	elif position.y > limits.get("y_max", INF):
+		_vel_b += Vector2.UP * border_strength
 
-	return vel_c + vel_a + vel_s + vel_b
+	var influence_vel = _vel_c + _vel_a + _vel_s + _vel_b
+	if is_nan(influence_vel.x) || is_nan(influence_vel.y):
+		return Vector2.ZERO
+	return influence_vel
+
+
+func _on_NeighbourArea_area_entered(area):
+	if area.is_in_group("boid_colliders"):
+		_neighbours.append(area.get_parent())
+
+
+func _on_NeighbourArea_area_exited(area):
+	if area.is_in_group("boid_colliders"):
+		_neighbours.remove(_neighbours.find(area.get_parent()))
--- scenes/BoidGroup.gd
+++ scenes/BoidGroup.gd
@@ -1,12 +1,9 @@
 extends Node2D
 
 export var initial_boids: int = 20
-export var draw_debug: bool = false
 
 var rng := RandomNumberGenerator.new()
 var boid_scene = preload("res://scenes/Boid.tscn")
-
-var boids = []
 
 onready var viewport_size := get_viewport_rect().size
 onready var limits := {"x_min": 0, "x_max": viewport_size.x, "y_min": 0, "y_max": viewport_size.y}
@@ -20,23 +17,14 @@ func _ready():
 	)
 
 
-func _process(delta: float):
-	for boid in boids:
-		boid.move(delta, boids, limits)
-
-
 func spawn_random_boids(n: int, x_min: int, x_max: int, y_min: int, y_max: int):
 	for _i in range(n):
-		var boid := spawn_boid(
-			rng.randi_range(x_min, x_max), rng.randi_range(y_min, y_max), draw_debug
-		)
-		boids.append(boid)
+		spawn_boid(rng.randi_range(x_min, x_max), rng.randi_range(y_min, y_max))
 
 
-func spawn_boid(x: int, y: int, draw: bool = false) -> Boid:
+func spawn_boid(x: int, y: int):
 	var boid_instance := boid_scene.instance() as Boid
-	boid_instance.draw = draw
-	boid_instance.global_position = Vector2(x, y)
-	boid_instance.velocity = Vector2.UP.rotated(rng.randf_range(0, 2 * PI)) * 1000
+	boid_instance.position = Vector2(x, y)
+	boid_instance.velocity = Vector2.UP.rotated(rng.randf_range(0, 2 * PI)) * 100
+	boid_instance.limits = limits
 	add_child(boid_instance)
-	return boid_instance
